3c1eaab237168830c944d24011e3461d3298c495
DaoVanSinh <Sinh2004tt@gmail.com>
2024-01-21 22:45:23 +0700

    Giaiphuongtrinhbacmot

---
+++ BAI_THUC_HANH_JAVA/src/Giaiphuongtrinhbacmot.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Giaiphuongtrinhbacmot {
+    public static void main(String[] args) {
+        double a ,b ;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap a: ");
+         a = scanner.nextDouble();
+
+        System.out.print("Nhap b: ");
+         b = scanner.nextDouble();
+
+        scanner.close();
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh co vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        } else {
+            double x = -b / a;
+            System.out.println("Nghiem cua phuong trinh la: x = " +x);
+        }
+        scanner.close();
+    }
+}
